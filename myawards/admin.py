e22fb24a64f8351cd5d0a44058dccfbc2026e375
Peter Njoroge <njoroge.pitz@gmail.com>
2018-11-19 15:15:03 +0300

    create admin

--- myawards/admin.py
+++ myawards/admin.py
@@ -1,6 +1,6 @@
-# from django.contrib import admin
-# from .models import Profile, Project
+from django.contrib import admin
+from .models import Profile, Project
 
-# # Register your models here.
-# admin.site.register(Profile)
-# admin.site.register(Project)
+# Register your models here.
+admin.site.register(Profile)
+admin.site.register(Project)
